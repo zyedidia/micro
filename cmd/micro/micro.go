00fe82e9c7c09bf2199fb738f3ff52b02fdbe9e4
Camille <onodera@openmailbox.org>
2016-05-27 23:39:53 +0200

    Fix typo

--- cmd/micro/micro.go
+++ cmd/micro/micro.go
@@ -19,7 +19,7 @@ import (
 const (
 	synLinesUp           = 75  // How many lines up to look to do syntax highlighting
 	synLinesDown         = 75  // How many lines down to look to do syntax highlighting
-	doubleClickThreshold = 400 // How many milliseconds to wait before a second click i not a double click
+	doubleClickThreshold = 400 // How many milliseconds to wait before a second click is not a double click
 	undoThreshold        = 500 // If two events are less than n milliseconds apart, undo both of them
 )
 
