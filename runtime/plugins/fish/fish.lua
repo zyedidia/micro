822696b11b3e884738b72b2c67ad6d4cc558443f
Camille <onodera@openmailbox.org>
2016-05-30 22:12:06 +0200

    Update for user-created-commands

--- runtime/plugins/fish/fish.lua
+++ runtime/plugins/fish/fish.lua
@@ -2,18 +2,21 @@ if GetOption("fishfmt") == nil then
     AddOption("fishfmt", false)
 end
 
+MakeCommand("fishfmt", "fish_fishfmt")
+
 function fish_onSave()
-    if view.Buf.FileType == "fish" then
+    if views[mainView+1].Buf.FileType == "fish" then
         if GetOption("fishfmt") then
             fish_fishfmt()
         end
-
-        view:ReOpen()
     end
 end
 
 function fish_fishfmt()
-    local handle = io.popen("fish_indent -w " .. view.Buf.Path)
+    views[mainView+1]:Save()
+    local handle = io.popen("fish_indent -w " .. views[mainView+1].Buf.Path)
     local result = handle:read("*a")
     handle:close()
+    
+    views[mainView+1]:ReOpen()
 end
