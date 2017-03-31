6041e063e253a05c00459c7d8e83f3c6261111e0
GeigerCounter <saturnstarinc2@gmail.com>
2017-03-31 05:22:32 -0400

    Added script for rpm and template rpmspec

---
+++ tools/compile-linux.sh
@@ -0,0 +1,32 @@
+# Source tar
+
+./vendor-src.sh micro-$1-src
+cd ..
+
+mkdir -p binaries
+mkdir -p micro-$1
+
+mv micro-$1-src.tar.gz binaries
+mv micro-$1-src.zip binaries
+
+cp LICENSE micro-$1
+cp README.md micro-$1
+cp LICENSE-THIRD-PARTY micro-$1
+
+HASH="$(git rev-parse --short HEAD)"
+VERSION="$(go run tools/build-version.go)"
+DATE="$(go run tools/build-date.go)"
+ADDITIONAL_GO_LINKER_FLAGS="$(go run tools/info-plist.go $VERSION)"
+
+echo "Linux 64"
+GOOS=linux GOARCH=amd64 go build -ldflags "-s -w -X main.Version=$1 -X main.CommitHash=$HASH -X 'main.CompileDate=$DATE'" -o micro-$1/micro ./cmd/micro
+tar -czf micro-$1-linux64.tar.gz micro-$1
+mv micro-$1-linux64.tar.gz binaries
+echo "Linux 32"
+GOOS=linux GOARCH=386 go build -ldflags "-s -w -X main.Version=$1 -X main.CommitHash=$HASH -X 'main.CompileDate=$DATE'" -o micro-$1/micro ./cmd/micro
+tar -czf micro-$1-linux32.tar.gz micro-$1
+mv micro-$1-linux32.tar.gz binaries
+echo "Linux arm"
+GOOS=linux GOARCH=arm go build -ldflags "-s -w -X main.Version=$1 -X main.CommitHash=$HASH -X 'main.CompileDate=$DATE'" -o micro-$1/micro ./cmd/micro
+tar -czf micro-$1-linux-arm.tar.gz micro-$1
+mv micro-$1-linux-arm.tar.gz binaries
