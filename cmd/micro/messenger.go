aac214e3a0e627f65c3617e2933e103d81894ce6
hidaruma <hidaruma@outlook.jp>
2017-07-20 21:16:08 +0900

    modified messenger.Display

--- cmd/micro/messenger.go
+++ cmd/micro/messenger.go
@@ -464,7 +464,7 @@ func (m *Messenger) Display() {
 	if m.hasMessage {
 		if m.hasPrompt || globalSettings["infobar"].(bool) {
 			runes := []rune(m.message + m.response)
-			posx = 0
+			posx := 0
 			for x := 0; x < len(runes); x++ {
 				screen.SetContent(posx, h-1, runes[x], nil, m.style)
 				posx += runewidth.RuneWidth(runes[x])
