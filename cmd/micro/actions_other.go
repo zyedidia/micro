4802403308b1ec81e0f1bc3e84b388bcdda6116b
Zachary Yedidia <zyedidia@gmail.com>
2017-12-10 16:15:06 -0500

    Remove android from actions_other build tag
    Ref #949

--- cmd/micro/actions_other.go
+++ cmd/micro/actions_other.go
@@ -1,9 +1,9 @@
-// +build android plan9 nacl windows
+// +build plan9 nacl windows
 
 package main
 
 func (v *View) Suspend(usePlugin bool) bool {
-	messenger.Error("Suspend is only supported on Linux")
+	messenger.Error("Suspend is only supported on Posix")
 
 	return false
 }
