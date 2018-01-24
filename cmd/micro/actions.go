9c98abbddcec91564031f7fde0d35d90af39b7f1
pql9132 <16060758+pql9132@users.noreply.github.com>
2018-01-24 06:34:52 -0800

    changed accessing buffer settings to Buf.Settings[]

--- cmd/micro/actions.go
+++ cmd/micro/actions.go
@@ -1112,7 +1112,7 @@ func (v *View) Undo(usePlugin bool) bool {
 		return false
 	}
 
-	if v.Buf.curCursor == 0 && GetGlobalOption("cursorundo").(bool) && GetLocalOption("cursorundo", v.Buf).(bool) {
+	if v.Buf.curCursor == 0 && v.Buf.Settings["cursorundo"].(bool) {
 		v.Buf.clearCursors()
 	}
 
