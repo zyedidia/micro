76d89ccd0a78cd9d1f64c40c928a1f12ac06ac62
Adrian Hesketh <adrianhesketh@hushmail.com>
2018-02-14 15:42:56 +0000

    Added period as deactivator.

--- cmd/micro/completer.go
+++ cmd/micro/completer.go
@@ -116,7 +116,7 @@ var defaultActivators = map[rune]int{
 	'A': -1, 'B': -1, 'C': -1, 'D': -1, 'E': -1, 'F': -1, 'G': -1, 'H': -1, 'I': -1, 'J': -1, 'K': -1, 'L': -1, 'M': -1, 'N': -1, 'O': -1, 'P': -1, 'Q': -1, 'R': -1, 'S': -1, 'T': -1, 'U': -1, 'V': -1, 'W': -1, 'X': -1, 'Y': -1, 'Z': -1,
 }
 
-const defaultDeactivators = "), \n"
+const defaultDeactivators = "), \n."
 
 // NewCompleterForView creates a new autocompleter with defaults for writing to the console.
 func NewCompleterForView(v *View) *Completer {
