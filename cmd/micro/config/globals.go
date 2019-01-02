b8b245f305516bd9abb3f8a50b177eea79b98390
Zachary Yedidia <zyedidia@gmail.com>
2019-01-02 17:39:50 -0500

    Add mouse support

---
+++ cmd/micro/config/globals.go
@@ -0,0 +1,6 @@
+package config
+
+const (
+	DoubleClickThreshold = 400 // How many milliseconds to wait before a second click is not a double click
+	AutosaveTime         = 8   // Number of seconds to wait before autosaving
+)
