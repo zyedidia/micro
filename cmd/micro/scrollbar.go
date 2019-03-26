050ad8a1fa78f450e21873a629c5dbbe88fece5b
Federico Palacios <fedenator7@gmail.com>
2019-03-25 23:18:39 -0300

    Improve scrollbar to consider the size of the file

--- cmd/micro/scrollbar.go
+++ cmd/micro/scrollbar.go
@@ -8,7 +8,29 @@ type ScrollBar struct {
 // Display shows the scrollbar
 func (sb *ScrollBar) Display() {
 	style := defStyle.Reverse(true)
-	screen.SetContent(sb.view.x+sb.view.Width-1, sb.view.y+sb.pos(), ' ', nil, style)
+
+	height := sb.height()
+
+	// If the height of the scrollbar is bigger than the view, don't render it
+	if height >= sb.view.Height {
+		return
+	} 
+
+	x := sb.view.x + sb.view.Width - 1
+	y := sb.view.y + sb.pos()
+
+	for i := 0; i < height; i++ {
+		screen.SetContent(x, y + i, ' ', nil, style)
+	}
+}
+
+func (sb *ScrollBar) height() int {
+	// The ratio between the height of the file and the height of the view
+	ratio := float32(sb.view.Buf.NumLines) / float32(sb.view.Height)
+
+	// Get height of the scrollbar so that the ratio between the view height
+	// and the scrollbar height have the same ratio
+	return int( float32(sb.view.Height) / ratio )
 }
 
 func (sb *ScrollBar) pos() int {
