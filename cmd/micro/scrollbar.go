551d900bcd4e8b49670d1d073d52b160b31fa047
fpalacios <fedenator7@gmail.com>
2019-05-22 12:38:33 -0300

    [TechDebt] gofmt scrollbar.go

--- cmd/micro/scrollbar.go
+++ cmd/micro/scrollbar.go
@@ -14,13 +14,13 @@ func (sb *ScrollBar) Display() {
 	// If the height of the scrollbar is bigger than the view, don't render it
 	if height >= sb.view.Height {
 		return
-	} 
+	}
 
 	x := sb.view.x + sb.view.Width - 1
 	y := sb.view.y + sb.pos()
 
 	for i := 0; i < height; i++ {
-		screen.SetContent(x, y + i, ' ', nil, style)
+		screen.SetContent(x, y+i, ' ', nil, style)
 	}
 }
 
@@ -30,7 +30,7 @@ func (sb *ScrollBar) height() int {
 
 	// Get height of the scrollbar so that the ratio between the view height
 	// and the scrollbar height have the same ratio
-	return int( float32(sb.view.Height) / ratio )
+	return int(float32(sb.view.Height) / ratio)
 }
 
 func (sb *ScrollBar) pos() int {
