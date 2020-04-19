ac296c78c0b31db4b483c66897ac4e0fe422dff6
2pac <tarasyarema@pm.me>
2020-04-19 18:57:27 +0200

    RunInteractiveShell: gofmt

--- internal/shell/shell.go
+++ internal/shell/shell.go
@@ -5,11 +5,11 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"os/exec"
 	"os/signal"
 	"strings"
-	"log"
 
 	shellquote "github.com/kballard/go-shellquote"
 	"github.com/zyedidia/micro/internal/screen"
@@ -79,98 +79,97 @@ func RunBackgroundShell(input string) (func() string, error) {
 //
 // TODO
 // Implement other type of command support?
-// egs. 
-//	If first ok: cmd1 && cmd2 
+// egs.
+//	If first ok: cmd1 && cmd2
 //  Sequential:  cmd1  & cmd2
 //  Concurrent:  cmd1  ; cmd2
-func executeCmdPipe(outputBuffer *bytes.Buffer, getOutput bool, stack ...*exec.Cmd) (err error) {
+func executeCmdPipe(outputBuffer *bytes.Buffer, getOutput bool, stack ...*exec.Cmd) (err error) {
 	if len(stack) == 0 {
 		return fmt.Errorf("length of the cmd stack is zero (0)")
-	} 
+	}
 
-    var errorBuffer bytes.Buffer
-    var pipeLen int
+	var errorBuffer bytes.Buffer
+	var pipeLen int
 	var pipes []*io.PipeWriter
 
 	// Handle the simple case, i.e. no pipes
 	if len(stack) == 1 {
 		pipeLen = 1
-		stack[0].Stdin = os.Stdin
-		
-		if getOutput {
-			stack[0].Stdout = io.MultiWriter(os.Stdout, outputBuffer)
-		} else {
-			stack[0].Stdout = os.Stdout
-		}
-		
+		stack[0].Stdin = os.Stdin
+
+		if getOutput {
+			stack[0].Stdout = io.MultiWriter(os.Stdout, outputBuffer)
+		} else {
+			stack[0].Stdout = os.Stdout
+		}
+
 		stack[0].Stderr = os.Stderr
 		stack[0].Start()
 
-	    log.Println("ExecuteCmdPipe: stack ->", stack)
-       	log.Println("ExecuteCmdPipe: pipes ->", pipes)
-       	log.Println("ExecuteCmdPipe: pipeLen     ->", pipeLen)
-       	log.Println("ExecuteCmdPipe: want output ->", getOutput)
+		log.Println("ExecuteCmdPipe: stack ->", stack)
+		log.Println("ExecuteCmdPipe: pipes ->", pipes)
+		log.Println("ExecuteCmdPipe: pipeLen     ->", pipeLen)
+		log.Println("ExecuteCmdPipe: want output ->", getOutput)
 
 		return stack[0].Wait()
 	}
 
-	pipeLen = len(stack)-1
-    pipes = make([]*io.PipeWriter, pipeLen)
+	pipeLen = len(stack) - 1
+	pipes = make([]*io.PipeWriter, pipeLen)
 
-    i := 0
-    for ; i < pipeLen; i++ {
-        stdinPipe, stdoutPipe := io.Pipe()
-        
-        stack[i].Stdout = stdoutPipe
-        stack[i].Stderr = &errorBuffer
+	i := 0
 
-     	stack[i+1].Stdin = stdinPipe
-        
-        pipes[i] = stdoutPipe
-    }
-    
-    if getOutput {
+	for ; i < pipeLen; i++ {
+		stdinPipe, stdoutPipe := io.Pipe()
+		stack[i].Stdout = stdoutPipe
+		stack[i].Stderr = &errorBuffer
+		stack[i+1].Stdin = stdinPipe
+		pipes[i] = stdoutPipe
+	}
+
+	if getOutput {
 		stack[i].Stdout = outputBuffer
 	} else {
 		stack[i].Stdout = os.Stdout
 	}
-    
-    stack[i].Stderr = os.Stderr
 
-    log.Println("ExecuteCmdPipe: stack ->", stack)
-  	log.Println("ExecuteCmdPipe: pipes ->", pipes)
-  	log.Println("ExecuteCmdPipe: pipeLen     ->", pipeLen)
-  	log.Println("ExecuteCmdPipe: want output ->", getOutput)
-
-    if err := executePipe(stack, pipes); err != nil {
-        return fmt.Errorf("executePipe(%s): %v", errorBuffer.Bytes(), err)
-    }
-    
-    return err
+	stack[i].Stderr = os.Stderr
+
+	log.Println("ExecuteCmdPipe: stack ->", stack)
+	log.Println("ExecuteCmdPipe: pipes ->", pipes)
+	log.Println("ExecuteCmdPipe: pipeLen     ->", pipeLen)
+	log.Println("ExecuteCmdPipe: want output ->", getOutput)
+
+	if err := executePipe(stack, pipes); err != nil {
+		return fmt.Errorf("executePipe(%s): %v", errorBuffer.Bytes(), err)
+	}
+
+	return err
+
 }
 
 // executePipe is a helper recursive function for ExecuteCmdPipe
-func executePipe(stack []*exec.Cmd, pipes []*io.PipeWriter) (err error) {
-    if stack[0].Process == nil {
-        if err = stack[0].Start(); err != nil {
-            return err
-        }
-    }
-    
-    if len(stack) > 1 {
-        if err = stack[1].Start(); err != nil {
-             return err
-        }
-        
-        defer func() {
-            if err == nil {
-                pipes[0].Close()
-                err = executePipe(stack[1:], pipes[1:])
-            }
-        }()
-    }
-    
-    return stack[0].Wait()
+func executePipe(stack []*exec.Cmd, pipes []*io.PipeWriter) (err error) {
+	if stack[0].Process == nil {
+		if err = stack[0].Start(); err != nil {
+			return err
+		}
+	}
+
+	if len(stack) > 1 {
+		if err = stack[1].Start(); err != nil {
+			return err
+		}
+
+		defer func() {
+			if err == nil {
+				pipes[0].Close()
+				err = executePipe(stack[1:], pipes[1:])
+			}
+		}()
+	}
+
+	return stack[0].Wait()
 }
 
 // RunInteractiveShell runs a shellcommand interactively
@@ -179,58 +178,58 @@ func RunInteractiveShell(input string, wait bool, getOutput bool) (string, error
 	if err != nil {
 		return "", err
 	}
-	
+
 	if len(args) == 0 {
 		return "", errors.New("No arguments")
 	}
 
 	commands := strings.Split(input, "|")
 
-	// Shut down the screen because we're going to interact 
+	// Shut down the screen because we're going to interact
 	// directly with the shell and defer the screen backup
 	screenb := screen.TempFini()
 	defer screen.TempStart(screenb)
-	
-   	outputBuffer := &bytes.Buffer{}
 
-	stack := make([]*exec.Cmd, len(commands)) 
+	outputBuffer := &bytes.Buffer{}
+
+	stack := make([]*exec.Cmd, len(commands))
 
 	for i := range commands {
 		iArgs, err := shellquote.Split(commands[i])
 		if err != nil {
 			return "", err
 		}
-		
+
 		stack[i] = exec.Command(iArgs[0], iArgs[1:]...)
 	}
-	
-   	log.Println("RunInteractiveShell: stack ->", stack)
+
+	log.Println("RunInteractiveShell: stack ->", stack)
 
 	c := make(chan os.Signal, 1)
-   	signal.Notify(c, os.Interrupt)
-   	go func() {
-   		for range c {
-   			for i := range args {
-   				if stack[i].Process != nil {
-   					stack[i].Process.Kill()
-   				}
-   			}
-   		}
-   	}()
-	
-   	err = executeCmdPipe(outputBuffer, getOutput, stack...)
-   	if err != nil {
-		log.Println("RunInteractiveShell: got error ->", err)
-   	}
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for range c {
+			for i := range args {
+				if stack[i].Process != nil {
+					stack[i].Process.Kill()
+				}
+			}
+		}
+	}()
 
-   	log.Println("RunInteractiveShell: outputBuffer ->", outputBuffer)
-   			
-	if wait {
-		// This is just so we don't return right away and let the user press enter to return
-		screen.TermMessage("")
+	err = executeCmdPipe(outputBuffer, getOutput, stack...)
+	if err != nil {
+		log.Println("RunInteractiveShell: got error ->", err)
 	}
-   	
-	return outputBuffer.String(), err;
+
+	log.Println("RunInteractiveShell: outputBuffer ->", outputBuffer)
+
+	if wait {
+		// This is just so we don't return right away and let the user press enter to return
+		screen.TermMessage("")
+	}
+
+	return outputBuffer.String(), err
 }
 
 // UserCommand runs the shell command
