132630a9a5e2e8d585b5d5de9e1110957d60c7e7
Zachary Yedidia <zyedidia@gmail.com>
2020-08-15 18:20:10 -0400

    Apply additional edits if they exist

--- internal/buffer/autocomplete.go
+++ internal/buffer/autocomplete.go
@@ -238,6 +238,14 @@ func LSPComplete(b *Buffer) []Completion {
 				Start: toLoc(item.TextEdit.Range.Start),
 				End:   toLoc(item.TextEdit.Range.End),
 			}}
+			for _, e := range item.AdditionalTextEdits {
+				d := Delta{
+					Text:  []byte(e.NewText),
+					Start: toLoc(e.Range.Start),
+					End:   toLoc(e.Range.End),
+				}
+				completions[i].Edits = append(completions[i].Edits, d)
+			}
 		} else {
 			var t string
 			if len(item.InsertText) > 0 {
