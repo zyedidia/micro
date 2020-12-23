zyedidia/micro

commit 3b34a021e3d30868248d95b19b86443a5dc5aab2

Improve file permission detection
Mark files as readonly automatically if write permission is denied.
Display errors when opening files (except for non-existence errors).

Fixes #1224

internal/buffer/buffer.go
@@ -212,8 +212,15 @@ func NewBufferFromFileAtLoc(path string, btype BufType, cursorLoc Loc) (*Buffer,
 		return nil, err
 	}
 
+	f, err := os.OpenFile(filename, os.O_WRONLY, 0)
+	readonly := os.IsPermission(err)
+	f.Close()
+
 	file, err := os.Open(filename)
-	fileInfo, _ := os.Stat(filename)
+	fileInfo, serr := os.Stat(filename)
+	if serr != nil {
+		return nil, serr
+	}
 
 	if err == nil && fileInfo.IsDir() {
 		return nil, errors.New("Error: " + filename + " is a directory and cannot be opened")
@@ -222,12 +229,16 @@ func NewBufferFromFileAtLoc(path string, btype BufType, cursorLoc Loc) (*Buffer,
 	defer file.Close()
 
 	var buf *Buffer
-	if err != nil {
+	if os.IsNotExist(err) {
 		// File does not exist -- create an empty buffer with that name
 		buf = NewBufferFromString("", filename, btype)
+	} else if err != nil {
+		return nil, err
 	} else {
 		buf = NewBuffer(file, util.FSize(file), filename, cursorLoc, btype)
 	}
+
+	buf.SetOptionNative("readonly", readonly)
 
 	return buf, nil
 }

commit ea58b6510fcdc54cbfa47bbb7435349c9b9d0910

Don't automatically disable readonly option
Fix the regression after 3b34a02: setting readonly option to true
in onBufferOpen lua callback doesn't work, since it is automatically
reset to false if write permission is not denied.

internal/buffer/buffer.go
@@ -238,7 +238,9 @@ func NewBufferFromFileAtLoc(path string, btype BufType, cursorLoc Loc) (*Buffer,
 		buf = NewBuffer(file, util.FSize(file), filename, cursorLoc, btype)
 	}
 
-	buf.SetOptionNative("readonly", readonly)
+	if readonly {
+		buf.SetOptionNative("readonly", true)
+	}
 
 	return buf, nil
 }