834fe3965c14eb967324e24482a0f0c47d467920
Adam Crossland <adam.crossland@gmail.com>
2022-01-03 16:29:37 -0500

    add tests for Encrypt and Decrypt

--- internal/util/util_test.go
+++ internal/util/util_test.go
@@ -31,3 +31,25 @@ func TestSliceVisualEnd(t *testing.T) {
 	assert.Equal(t, []byte("ello"), slc)
 	assert.Equal(t, 0, n)
 }
+
+func TestEncrypt(t *testing.T) {
+	result, err := Encrypt("This is a test string to encrypt", "thisisasamplekey")
+	assert.Nil(t, err)
+	assert.NotEmpty(t, result)
+}
+
+func TestEncryptAndDecrypt(t *testing.T) {
+	encryptResult, err := Encrypt("This is a test string to encrypt", "thisisasamplekey")
+	assert.Nil(t, err)
+	assert.NotEmpty(t, encryptResult)
+
+	decryptResult, err := Decrypt(encryptResult, "thisisasamplekey")
+	assert.Nil(t, err)
+	assert.Equal(t, "This is a test string to encrypt", decryptResult)
+}
+
+func TestEncryptWithEmptyKey(t *testing.T) {
+	result, err := Encrypt("This is a test string to encrypt", "")
+	assert.NotNil(t, err)
+	assert.Equal(t, "encryption key cannot be empty", result)
+}
