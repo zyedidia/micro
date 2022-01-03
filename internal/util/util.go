afa36762bf5c103399c164f39bdc486539bba2b4
Adam Crossland <adam.crossland@gmail.com>
2022-01-03 14:48:34 -0500

    check for zero-length keys before encrypting

--- internal/util/util.go
+++ internal/util/util.go
@@ -500,46 +500,56 @@ func Unzip(src, dest string) error {
 }
 
 func Encrypt(text, key string) (string, error) {
-	keyHash := sha256.Sum256([]byte(key))
-	c, err := aes.NewCipher(keyHash[:])
-	if err != nil {
-		return fmt.Sprintf("creating cipher err: %v", err), err
-	}
-	gcm, err := cipher.NewGCM(c)
-	if err != nil {
-		return fmt.Sprintf("creating gcm err: %v", err), err
-	}
-	nonce := make([]byte, gcm.NonceSize())
-	// populates our nonce with a cryptographically secure
-	// random sequence
-	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-		fmt.Println(err)
-	}
+	if len(key) > 0 {
+		keyHash := sha256.Sum256([]byte(key))
+		c, err := aes.NewCipher(keyHash[:])
+		if err != nil {
+			return fmt.Sprintf("creating cipher err: %v", err), err
+		}
+		gcm, err := cipher.NewGCM(c)
+		if err != nil {
+			return fmt.Sprintf("creating gcm err: %v", err), err
+		}
+		nonce := make([]byte, gcm.NonceSize())
+		// populates our nonce with a cryptographically secure
+		// random sequence
+		if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+			fmt.Println(err)
+		}
+		if err != nil {
+			return fmt.Sprintf("creating nonce err: %v", err), err
+		}
 
-	// return hex string
-	return hex.EncodeToString(gcm.Seal(nonce, nonce, []byte(text), nil)), nil
+		// return hex string
+		return hex.EncodeToString(gcm.Seal(nonce, nonce, []byte(text), nil)), nil
+	} else {
+		return "encryption key cannot be empty", fmt.Errorf("encryption key cannot be empty")
+	}
 }
 
 func Decrypt(text, key string) (string, error) {
-	keyHash := sha256.Sum256([]byte(key))
-	c, err := aes.NewCipher(keyHash[:])
-	if err != nil {
-		return fmt.Sprintf("creating cipher err: %v", err), err
-	}
-	gcm, err := cipher.NewGCM(c)
-	if err != nil {
-		return fmt.Sprintf("creating gcm err: %v", err), err
-	}
+	if len(key) > 0 {
+		keyHash := sha256.Sum256([]byte(key))
+		c, err := aes.NewCipher(keyHash[:])
+		if err != nil {
+			return fmt.Sprintf("creating cipher err: %v", err), err
+		}
+		gcm, err := cipher.NewGCM(c)
+		if err != nil {
+			return fmt.Sprintf("creating gcm err: %v", err), err
+		}
 
-	textAsBytes, err := hex.DecodeString(text)
-	if err != nil {
-		return fmt.Sprintf("decoding err: %v", err), err
-	}
-	nonce, ciphertext := textAsBytes[:gcm.NonceSize()], textAsBytes[gcm.NonceSize():]
-	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
-	if err == nil {
+		textAsBytes, err := hex.DecodeString(text)
+		if err != nil {
+			return fmt.Sprintf("decoding err: %v", err), err
+		}
+		nonce, ciphertext := textAsBytes[:gcm.NonceSize()], textAsBytes[gcm.NonceSize():]
+		plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+		if err != nil {
+			return fmt.Sprintf("decrypting err: %v", err), err
+		}
 		return string(plaintext), nil
 	} else {
-		return fmt.Sprintf("decrypting err: %v", err), err
+		return "encryption key cannot be empty", fmt.Errorf("encryption key cannot be empty")
 	}
 }
