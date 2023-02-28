e39b995c4ada716969be4ff9ef42eaea81e6aa1f
154pinkchairs <marcelina.holub@appunite.com>
2023-02-28 10:12:03 +0100

    test(runtime): remove cwd checks

--- runtime/runtime_test.go
+++ runtime/runtime_test.go
@@ -1,23 +1,13 @@
 package config
 
 import (
-	"os"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func TestAssetDir(t *testing.T) {
 	t.Parallel()
-	// Change working directory to runtime if needed
-	cwd, err := os.Getwd()
-	assert.NoError(t, err)
-	if !strings.Contains(cwd, "runtime") {
-		err := os.Chdir("./runtime")
-		require.NoError(t, err)
-	}
 	// Test AssetDir
 	entries, err := AssetDir("syntax")
 	assert.NoError(t, err)
