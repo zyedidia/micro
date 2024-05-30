300d6fe2e4d7a583fb67ad9f2b03b09e380ea375
Jöran Karl <3951388+JoeKar@users.noreply.github.com>
2024-05-30 21:34:11 +0200

    ioutil: Remove deprecated functions where possible

--- internal/config/settings.go
+++ internal/config/settings.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"reflect"
@@ -217,7 +216,7 @@ func ReadSettings() error {
 	parsedSettings = make(map[string]interface{})
 	filename := filepath.Join(ConfigDir, "settings.json")
 	if _, e := os.Stat(filename); e == nil {
-		input, err := ioutil.ReadFile(filename)
+		input, err := os.ReadFile(filename)
 		if err != nil {
 			settingsParseError = true
 			return errors.New("Error reading settings.json file: " + err.Error())
@@ -342,7 +341,7 @@ func WriteSettings(filename string) error {
 		}
 
 		txt, _ := json.MarshalIndent(parsedSettings, "", "    ")
-		err = ioutil.WriteFile(filename, append(txt, '\n'), util.FileMode)
+		err = os.WriteFile(filename, append(txt, '\n'), util.FileMode)
 	}
 	return err
 }
@@ -364,7 +363,7 @@ func OverwriteSettings(filename string) error {
 		}
 
 		txt, _ := json.MarshalIndent(settings, "", "    ")
-		err = ioutil.WriteFile(filename, append(txt, '\n'), util.FileMode)
+		err = os.WriteFile(filename, append(txt, '\n'), util.FileMode)
 	}
 	return err
 }
