89507ce5592b74f20dcfcd880f17574e8ba0f806
Jöran Karl <3951388+JoeKar@users.noreply.github.com>
2024-05-31 20:44:38 +0200

    backup: Use new `SafeWrite()`

--- internal/buffer/save.go
+++ internal/buffer/save.go
@@ -120,6 +120,9 @@ func (b *Buffer) Overwrite(name string, isBackup bool, withSudo bool) (err error
 
 	var size int
 	fwriter := func(file io.Writer) (e error) {
+		b.Lock()
+		defer b.Unlock()
+
 		if len(b.lines) == 0 {
 			return
 		}
