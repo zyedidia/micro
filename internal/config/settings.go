367589049d20bcfff38e77e99d322fa76cffa19b
Jöran Karl <3951388+JoeKar@users.noreply.github.com>
2024-06-01 16:38:57 +0200

    settings: Perform write process atomic

--- internal/config/settings.go
+++ internal/config/settings.go
@@ -150,6 +150,10 @@ var (
 	VolatileSettings map[string]bool
 )
 
+func writeFile(name string, txt []byte) error {
+	return util.SafeWrite(name, txt, false)
+}
+
 func init() {
 	ModifiedSettings = make(map[string]bool)
 	VolatileSettings = make(map[string]bool)
@@ -341,7 +345,8 @@ func WriteSettings(filename string) error {
 		}
 
 		txt, _ := json.MarshalIndent(parsedSettings, "", "    ")
-		err = os.WriteFile(filename, append(txt, '\n'), util.FileMode)
+		txt = append(txt, '\n')
+		err = writeFile(filename, txt)
 	}
 	return err
 }
@@ -362,8 +367,9 @@ func OverwriteSettings(filename string) error {
 			}
 		}
 
-		txt, _ := json.MarshalIndent(settings, "", "    ")
-		err = os.WriteFile(filename, append(txt, '\n'), util.FileMode)
+		txt, _ := json.MarshalIndent(parsedSettings, "", "    ")
+		txt = append(txt, '\n')
+		err = writeFile(filename, txt)
 	}
 	return err
 }
