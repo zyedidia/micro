89ba95b9e3d97dcfa3a9d708fb1d0e17a72163e1
Jöran Karl <3951388+JoeKar@users.noreply.github.com>
2024-12-28 14:14:00 +0100

    actions: SaveCmd: Print the error of `SaveAs` to the `InfoBar`

--- internal/action/command.go
+++ internal/action/command.go
@@ -889,7 +889,10 @@ func (h *BufPane) SaveCmd(args []string) {
 	if len(args) == 0 {
 		h.Save()
 	} else {
-		h.Buf.SaveAs(args[0])
+		err := h.Buf.SaveAs(args[0])
+		if err != nil {
+			InfoBar.Error(err)
+		}
 	}
 }
 
