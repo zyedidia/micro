7aed46dc23c2e7adc5c3ff43ceea2b719f313562
Neko Box Coder <nekoboxcoder@gmail.com>
2025-03-12 23:52:02 +0000

    Adding lua function to use result of updated RunBackgroundShell

--- cmd/micro/initlua.go
+++ cmd/micro/initlua.go
@@ -101,6 +101,14 @@ func luaImportMicroShell() *lua.LTable {
 	ulua.L.SetField(pkg, "ExecCommand", luar.New(ulua.L, shell.ExecCommand))
 	ulua.L.SetField(pkg, "RunCommand", luar.New(ulua.L, shell.RunCommand))
 	ulua.L.SetField(pkg, "RunBackgroundShell", luar.New(ulua.L, shell.RunBackgroundShell))
+	ulua.L.SetField(pkg, "LaunchBackgroundShellFunc", luar.New(ulua.L, func(f func() (string, error), cb func(string, error)) {
+		go func() {
+			output, runErr := f()
+			if cb != nil {
+				cb(output, runErr)
+			}
+		}()
+	}))
 	ulua.L.SetField(pkg, "RunInteractiveShell", luar.New(ulua.L, shell.RunInteractiveShell))
 	ulua.L.SetField(pkg, "JobStart", luar.New(ulua.L, shell.JobStart))
 	ulua.L.SetField(pkg, "JobSpawn", luar.New(ulua.L, shell.JobSpawn))
