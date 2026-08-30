zyedidia/micro

commit 4c47a2713aafdb105d5a45ca0f859d0e7290f617

Autocomplete support for multiple cursors (#4028)
Co-authored-by: redsti <redsti@redsti.net>

internal/action/actions.go
@@ -916,16 +916,6 @@ func (h *BufPane) Autocomplete() bool {
 		return true
 	}
 
-	if h.Cursor.X == 0 {
-		return false
-	}
-	r := h.Cursor.RuneUnder(h.Cursor.X)
-	prev := h.Cursor.RuneUnder(h.Cursor.X - 1)
-	if !util.IsAutocomplete(prev) || util.IsWordChar(r) {
-		// don't autocomplete if cursor is within a word
-		return false
-	}
-
 	return b.Autocomplete(buffer.BufferComplete)
 }
 

internal/buffer/autocomplete.go
@@ -25,6 +25,9 @@ func (b *Buffer) GetSuggestions() {
 
 // Autocomplete starts the autocomplete process
 func (b *Buffer) Autocomplete(c Completer) bool {
+	if !b.GetActiveCursor().CanAutocomplete() {
+		return false
+	}
 	b.Completions, b.Suggestions = c(b)
 	if len(b.Completions) != len(b.Suggestions) || len(b.Completions) == 0 {
 		return false
@@ -49,23 +52,57 @@ func (b *Buffer) CycleAutocomplete(forward bool) {
 		b.CurSuggestion = len(b.Suggestions) - 1
 	}
 
-	c := b.GetActiveCursor()
-	start := c.Loc
-	end := c.Loc
-	if prevSuggestion < len(b.Suggestions) && prevSuggestion >= 0 {
-		start = end.Move(-util.CharacterCountInString(b.Completions[prevSuggestion]), b)
+	tmpWord, _ := b.GetWord()
+	activeWord := make([]byte, len(tmpWord))
+	copy(activeWord, tmpWord)
+	for _, c := range b.cursors {
+		if !c.CanAutocomplete() {
+			continue
+		}
+
+		word, _ := c.GetWord()
+		if !bytes.Equal(word, activeWord) {
+			continue
+		}
+
+		c.autocomplete(prevSuggestion)
 	}
 
-	b.Replace(start, end, b.Completions[b.CurSuggestion])
 	if len(b.Suggestions) > 1 {
 		b.HasSuggestions = true
 	}
 }
 
+func (c *Cursor) autocomplete(prevSuggestion int) {
+	b := c.buf
+	start := c.Loc
+	end := c.Loc
+
+	if prevSuggestion < len(b.Suggestions) && prevSuggestion >= 0 {
+		start = end.Move(-util.CharacterCountInString(b.Completions[prevSuggestion]), b)
+	}
+
+	b.Replace(start, end, b.Completions[b.CurSuggestion])
+}
+
+func (c *Cursor) CanAutocomplete() bool {
+	if c.X == 0 {
+		return false
+	}
+
+	r := c.RuneUnder(c.X)
+	prev := c.RuneUnder(c.X - 1)
+	if !util.IsAutocomplete(prev) || util.IsWordChar(r) {
+		// don't autocomplete if cursor is within a word
+		return false
+	}
+	return true
+}
+
 // GetWord gets the most recent word separated by any separator
 // (whitespace, punctuation, any non alphanumeric character)
-func (b *Buffer) GetWord() ([]byte, int) {
-	c := b.GetActiveCursor()
+func (c *Cursor) GetWord() ([]byte, int) {
+	b := c.buf
 	l := b.LineBytes(c.Y)
 	l = util.SliceStart(l, c.X)
 
@@ -80,6 +117,10 @@ func (b *Buffer) GetWord() ([]byte, int) {
 	args := bytes.FieldsFunc(l, util.IsNonWordChar)
 	input := args[len(args)-1]
 	return input, c.X - util.CharacterCount(input)
+}
+
+func (b *Buffer) GetWord() ([]byte, int) {
+	return b.GetActiveCursor().GetWord()
 }
 
 // GetArg gets the most recent word (separated by ' ' only)
@@ -153,7 +194,7 @@ func FileComplete(b *Buffer) ([]string, []string) {
 // BufferComplete autocompletes based on previous words in the buffer
 func BufferComplete(b *Buffer) ([]string, []string) {
 	c := b.GetActiveCursor()
-	input, argstart := b.GetWord()
+	input, argstart := c.GetWord()
 
 	if argstart == -1 {
 		return []string{}, []string{}

commit 55305c140ee3b1f8b617b4d4c122c475f6592332

Fixing commandbar/plugin autocomplete regression from #4028
PR #4028 moved the check for autocomplete from action autocomplete to
the buffer one, which caused a few regression for upstream callers
(commandbar autocomplete & plugins that use buffer autocomplete)
that use the buffer autocomplete.

This commit restores pre #4028 behavior by adding a callback variant
for buffer autocomplete functions while keeping the multi-cursor
autocomplete function.

internal/action/actions.go
@@ -903,6 +903,20 @@ func (h *BufPane) OutdentSelection() bool {
 	return false
 }
 
+func canAutocomplete(c *buffer.Cursor) bool {
+	if c.X == 0 {
+		return false
+	}
+
+	r := c.RuneUnder(c.X)
+	prev := c.RuneUnder(c.X - 1)
+	if !util.IsAutocomplete(prev) || util.IsWordChar(r) {
+		// don't autocomplete if cursor is within a word
+		return false
+	}
+	return true
+}
+
 // Autocomplete cycles the suggestions and performs autocompletion if there are suggestions
 func (h *BufPane) Autocomplete() bool {
 	b := h.Buf
@@ -916,7 +930,7 @@ func (h *BufPane) Autocomplete() bool {
 		return true
 	}
 
-	return b.Autocomplete(buffer.BufferComplete)
+	return b.AutocompleteCB(buffer.BufferComplete, canAutocomplete)
 }
 
 // CycleAutocompleteBack cycles back in the autocomplete suggestion list
@@ -926,7 +940,7 @@ func (h *BufPane) CycleAutocompleteBack() bool {
 	}
 
 	if h.Buf.HasSuggestions {
-		h.Buf.CycleAutocomplete(false)
+		h.Buf.CycleAutocompleteCB(false, canAutocomplete)
 		return true
 	}
 	return false

internal/buffer/autocomplete.go
@@ -25,20 +25,29 @@ func (b *Buffer) GetSuggestions() {
 
 // Autocomplete starts the autocomplete process
 func (b *Buffer) Autocomplete(c Completer) bool {
-	if !b.GetActiveCursor().CanAutocomplete() {
+	return b.AutocompleteCB(c, nil)
+}
+
+func (b *Buffer) AutocompleteCB(c Completer, checkCB func(*Cursor) bool) bool {
+	if checkCB != nil && !checkCB(b.GetActiveCursor()) {
 		return false
 	}
 	b.Completions, b.Suggestions = c(b)
+
 	if len(b.Completions) != len(b.Suggestions) || len(b.Completions) == 0 {
 		return false
 	}
 	b.CurSuggestion = -1
-	b.CycleAutocomplete(true)
+	b.CycleAutocompleteCB(true, checkCB)
 	return true
 }
 
 // CycleAutocomplete moves to the next suggestion
 func (b *Buffer) CycleAutocomplete(forward bool) {
+	b.CycleAutocompleteCB(forward, nil)
+}
+
+func (b *Buffer) CycleAutocompleteCB(forward bool, checkCB func(*Cursor) bool) {
 	prevSuggestion := b.CurSuggestion
 
 	if forward {
@@ -56,7 +65,7 @@ func (b *Buffer) CycleAutocomplete(forward bool) {
 	activeWord := make([]byte, len(tmpWord))
 	copy(activeWord, tmpWord)
 	for _, c := range b.cursors {
-		if !c.CanAutocomplete() {
+		if checkCB != nil && !checkCB(c) {
 			continue
 		}
 
@@ -83,20 +92,6 @@ func (c *Cursor) autocomplete(prevSuggestion int) {
 	}
 
 	b.Replace(start, end, b.Completions[b.CurSuggestion])
-}
-
-func (c *Cursor) CanAutocomplete() bool {
-	if c.X == 0 {
-		return false
-	}
-
-	r := c.RuneUnder(c.X)
-	prev := c.RuneUnder(c.X - 1)
-	if !util.IsAutocomplete(prev) || util.IsWordChar(r) {
-		// don't autocomplete if cursor is within a word
-		return false
-	}
-	return true
 }
 
 // GetWord gets the most recent word separated by any separator